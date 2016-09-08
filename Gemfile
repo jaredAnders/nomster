d9b8080a17aa62a243bdaf2510e6a1c477f9f509
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-09-08 14:05:33 +0000

    adds paper theme bootswatch, customizes variables, adds simple form, builds new place form

--- Gemfile
+++ Gemfile
@@ -41,3 +41,5 @@ gem 'spring',        group: :development
 gem 'rails_12factor', group: :production
 gem 'autoprefixer-rails'
 gem 'kaminari'
+gem 'bootswatch-rails'
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,9 @@ class PlacesController < ApplicationController
   def index
     @places = Place.order(:created_at).reverse_order.page(params[:page]).per(3)
   end
+
+  def new
+    @place = Place.new
+  end
+
 end
