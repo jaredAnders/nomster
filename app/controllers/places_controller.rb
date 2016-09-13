817d7beb745996ae447c1434393da6430662f1a8
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-09-13 02:03:15 +0000

    prevents users from editing places submitted by other users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.order(:created_at).reverse_order.page(params[:page]).per(3)
@@ -20,10 +20,19 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render :file => "#{Rails.root}/public/404.html",  :status => 404
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render :file => "#{Rails.root}/public/404.html",  :status => 404
+    end
+
     @place.update_attributes(place_params)
     redirect_to place_path(@place)
   end
