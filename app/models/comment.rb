617469151073a0a0b6e232fdc5f5a70247cfbf0c
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-09-20 21:58:59 +0000

    adds rating dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    '1'    => '1_star',
+    '2'   => '2_stars',
+    '3' => '3_stars',
+    '4'  => '4_stars',
+    '5'  => '5_stars'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
