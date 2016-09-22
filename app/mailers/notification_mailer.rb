85c5b4659dcd7533245fc4efcac8860a71de6a22
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-09-22 13:04:59 +0000

    configures notifications to be sent from test gmail account

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "info@nomstrocity.com"
 
   def comment_added
-    mail(to: "jikuyi@cartelera.org",
+    mail(to: "neclacusta@throwam.com",
           subject: "A new comment has been added")
   end
 
