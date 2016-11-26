65af7d6487e9870f6982f7335bcb5c768a9534e8
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-26 12:56:40 -0500

    fixes photo caption and removes from helper

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,11 +1,2 @@
 module PlacesHelper
-  def photo_caption(photo)
-    html = ""
-      if photo.caption.present?
-        html+= '<div class="carousel-caption">'
-        html+= photo.caption
-        html+= '</div>'
-      end
-    html
-  end
 end
